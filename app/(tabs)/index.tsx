19c547d9394b199c906d7160655e0b38239e9903
“dyva” <dyva@zero-one-group.com>
2025-02-05 10:49:22 +0700

    fix: add tamagui conf

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,49 +1,106 @@
-import { View, Text } from 'react-native';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { useState } from 'react';
-import Slider from '@react-native-community/slider'
+import React from 'react';
+import {
+  Accordion,
+  AlertDialog,
+  Button,
+  Paragraph,
+  ScrollView,
+  Square,
+  XStack,
+  YStack,
+} from 'tamagui';
 
 export default function HomeScreen() {
-  const [value, setValue] = useState<number>(1);
-
-  const renderTiles = () => {
-    const size = 15;
-    return Array.from({ length: value}).map((_, index) => {
-      return <View
-      key={index}
-      style={{
-        width: size,
-        height: size,
-        borderWidth: 0.5,
-        borderColor: 'white',
-        backgroundColor: '#3268a8'
-      }}
-    />
-    })
-  }
-
   return (
-    <ParallaxScrollView headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}>
-      <Text style={{textAlign: 'center'}}>
-        <Text style={{fontWeight: 500}}>{value}</Text> Tiles Rendered
-      </Text>
-      <Slider
-        value={1}
-        minimumValue={1}
-        maximumValue={1000}
-        step={1}
-        onValueChange={newValue => {
-          setValue(newValue);
-        }}
-      />
-      <View
-        style={{
-          flexDirection: 'row',
-          flex: 1,
-          flexWrap: 'wrap',
-        }}>
-        {renderTiles()}
-      </View>
-    </ParallaxScrollView>
+    <ScrollView width="100%" backgroundColor="$background" padding="$5" borderRadius="$4">
+      <Accordion overflow="hidden" width="100%" type="multiple">
+        <Accordion.Item value="a1">
+          <Accordion.Trigger flexDirection="row" justifyContent="space-between">
+            {({ open }: { open: boolean }) => (
+              <>
+                <Paragraph>1. Take a cold shower</Paragraph>
+                <Square animation="quick" rotate={open ? '180deg' : '0deg'} />
+              </>
+            )}
+          </Accordion.Trigger>
+          <Accordion.HeightAnimator animation="bouncy">
+            <Accordion.Content animation="bouncy" exitStyle={{ opacity: 0 }}>
+              <Paragraph>
+                Cold showers can help reduce inflammation, relieve pain, improve circulation, lower
+                stress levels, and reduce muscle soreness and fatigue.
+              </Paragraph>
+            </Accordion.Content>
+          </Accordion.HeightAnimator>
+        </Accordion.Item>
+
+        <Accordion.Item value="a2">
+          <Accordion.Trigger flexDirection="row" justifyContent="space-between">
+            {({ open }: { open: boolean }) => (
+              <>
+                <Paragraph>2. Eat 4 eggs</Paragraph>
+                <Square animation="quick" rotate={open ? '180deg' : '0deg'} />
+              </>
+            )}
+          </Accordion.Trigger>
+          <Accordion.HeightAnimator animation="bouncy">
+            <Accordion.Content animation="bouncy" exitStyle={{ opacity: 0 }}>
+              <Paragraph>
+                Eggs have been a dietary staple since time immemorial and there’s good reason for
+                their continued presence in our menus and meals.
+              </Paragraph>
+            </Accordion.Content>
+          </Accordion.HeightAnimator>
+        </Accordion.Item>
+      </Accordion>
+      <AlertDialog native>
+        <AlertDialog.Trigger asChild>
+          <Button>Show Alert</Button>
+        </AlertDialog.Trigger>
+
+        <AlertDialog.Portal>
+          <AlertDialog.Overlay
+            key="overlay"
+            animation="quick"
+            opacity={0.5}
+            enterStyle={{ opacity: 0 }}
+            exitStyle={{ opacity: 0 }}
+          />
+          <AlertDialog.Content
+            bordered
+            elevate
+            key="content"
+            animation={[
+              'quick',
+              {
+                opacity: {
+                  overshootClamping: true,
+                },
+              },
+            ]}
+            enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
+            exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
+            x={0}
+            scale={1}
+            opacity={1}
+            y={0}>
+            <YStack space>
+              <AlertDialog.Title>Accept</AlertDialog.Title>
+              <AlertDialog.Description>
+                By pressing yes, you accept our terms and conditions.
+              </AlertDialog.Description>
+
+              <XStack gap="$3" justifyContent="flex-end">
+                <AlertDialog.Cancel asChild>
+                  <Button>Cancel</Button>
+                </AlertDialog.Cancel>
+                <AlertDialog.Action asChild>
+                  <Button theme="active">Accept</Button>
+                </AlertDialog.Action>
+              </XStack>
+            </YStack>
+          </AlertDialog.Content>
+        </AlertDialog.Portal>
+      </AlertDialog>
+    </ScrollView>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,22 @@
+import '../tamagui-web.css';
+
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { useColorScheme } from 'react-native';
+import { TamaguiProvider } from 'tamagui';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import tamaguiConfig from '../tamagui.config';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </ThemeProvider>
+    </TamaguiProvider>
   );
 }
